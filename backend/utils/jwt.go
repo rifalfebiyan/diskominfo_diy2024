7d447726d251359e55ea754f8aa9be1f896c8bb5
Haidir Abdillah <haidirabdillah9@gmail.com>
2024-10-28 14:27:53 +0700

    user

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -1,12 +1,13 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5" // menggunakan package jwt yang baru
 )
 
-var jwtKey = []byte("your_secret_key") // ganti dengan secret key yang aman
+var jwtKey = []byte("your_secret_key") // Ganti dengan secret key yang aman
 
 func GenerateToken(userID uint, role string) (string, error) {
 	claims := jwt.MapClaims{
@@ -21,6 +22,9 @@ func GenerateToken(userID uint, role string) (string, error) {
 
 func ValidateToken(tokenString string) (*jwt.Token, error) {
 	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
 		return jwtKey, nil
 	})
 }
